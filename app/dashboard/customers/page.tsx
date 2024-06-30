3ee6bf83b24c61c2e7de2fc2bd6a5b6f803888ce
felixateya <mulaafelix6@gmail.com>
2024-06-30 11:10:13 +0300

    minor changes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
-export default function Customers() {
-    return <p>Customers Page</p>
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Customers | Acme Dashboard',
+};
+export default async function Customers() {
+  const customers = await fetchFilteredCustomers('');
+  return <CustomersTable customers={customers} />;
 }
